857e7bb09783e62ad50777c2f76ab18960d352e5
Andrea Romanello <rommelandrea@gmail.com>
2011-10-28 11:43:46 +0200

    inizializzazione

---
+++ sorting.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   sorting.c
+ * Author: rommel
+ *
+ * Created on October 28, 2011, 11:42 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
